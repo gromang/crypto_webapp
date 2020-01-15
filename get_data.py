d493f0244be98db6201c1aea1e876dab843e7625
Roman Moiseev <lp15.mr@yandex.ru>
2020-01-15 17:33:06 +0300

    функция получения последней записи

--- get_data.py
+++ get_data.py
@@ -8,7 +8,7 @@ from datetime import datetime
 from config import dbsettings
 
 now = datetime.now()
-logging.basicConfig(filename=f"{os.getcwd()}/logs/{now.strftime('%Y-%m-%d__%H')}.log", level=logging.INFO,
+logging.basicConfig(filename=f"{os.getcwd()}\logs\{now.strftime('%Y-%m-%d__%H')}.log", level=logging.INFO,
                     filemode="a", format='%(levelname)s %(asctime)s : %(message)s')
 logging.info(f'-------------------{now}-------------------')
 
@@ -19,65 +19,35 @@ class CryptoData:
         self.interval = interval
         self.depth = depth
 
-    def get_previous_candle_time(self):
-        try:
-            get_utc_time = requests.get("https://yandex.com/time/sync.json")
-            get_utc_time.raise_for_status()
-        except (
-            requests.exceptions.RequestException,
-            requests.exceptions.HTTPError,
-        ) as err:
-            logging.error(f"GET TIME ERROR: {err}")
-            return False
-        get_utc_time = get_utc_time.json()
-        # Забираем и форматируем Timestamp (отбрасываем милисекунды)
-        utc_ts = int(str(get_utc_time["time"])[:-3])
-        # Получаем текущую минуту
-        current_minute_time = datetime.fromtimestamp(
-            utc_ts).strftime("%Y-%m-%d %H:%M")
-        # Получаем timestamp предыдущей минуты
-        ts = int(
-            datetime.strptime(current_minute_time,
-                              "%Y-%m-%d %H:%M").timestamp() - 60
-        )
-        logging.info(f"Formed candle time: {ts}")
-        return ts
-
-    def connection_to_base():
-        conn = psycopg2.connect(database=database['database'],
-                                user=database['user'],
-                                password=database['password'],
-                                host=database['host'],
-                                port=database['port'],
+    def connection_to_base(self):
+        conn = psycopg2.connect(database=dbsettings['database'],
+                                user=dbsettings['user'],
+                                password=dbsettings['password'],
+                                host=dbsettings['host'],
+                                port=dbsettings['port'],
                                 )
-
-        print("База данных успешно открыта")
         return conn
 
-    def retrieving_table_data(table_database: str):
-        my_list = []
-        with connection_to_base() as conn:
+    def last_data_time(self, table_name: str):
+        with self.connection_to_base() as conn:
             with conn.cursor() as curs:
-                curs.execute(
-                    f'''SELECT * FROM "{table_database}" '''
-                )
+                curs.execute(f'''SELECT * FROM data_btc ORDER BY "{table_name}" DESC LIMIT 1 ''')
+                last_timestamp = curs.fetchone()[1]
+                last_time = datetime.fromtimestamp(last_timestamp).strftime("%Y-%m-%d %H:%M"))
+                # value = datetime.fromtimestamp(record[1])
+                # my_list.append({
+                #     "id": record[0],
+                #     "Timestamp": value.strftime('%d-%m-%Y %H:%M:%S'),
+                #     "Open": float(record[2]),
+                #     "Close": float(record[3]),
+                #     "High": float(record[4]),
+                #     "Low": float(record[5]),
+                #     "Volume": float(record[6])
+                # })
+        return last_time
 
-                for record in curs:
-                    value = datetime.fromtimestamp(record[1])
-                    my_list.append({
-                        "id": record[0],
-                        "Timestamp": value.strftime('%d-%m-%Y %H:%M:%S'),
-                        "Open": float(record[2]),
-                        "Close": float(record[3]),
-                        "High": float(record[4]),
-                        "Low": float(record[5]),
-                        "Volume": float(record[6])
-                    })
-
-        print('Таблица успешно прочитана')
-        conn.close()
-        return my_list
 
 
 if __name__ == "__main__":
-    retrieving_table_data('crypto_project')
+    test = CryptoData("BTCUSD", 1, 50)
+    test.last_data_time("data_btc")
